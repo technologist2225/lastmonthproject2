83932d5fd3d5d5ef1560ec0b31314a31ff556312
technologist2225 <130536324+technologist2225@users.noreply.github.com>
2026-04-17 18:06:31 +0700

    Delete product_manager directory

--- product_manager/app/models.py
+++
@@ -1,8 +0,0 @@
-class Product:
-    def __init__(self, id, name, price):
-        self.id = id
-        self.name = name
-        self.price = price
-
-    def to_dict(self):
-        return {"id": self.id, "name": self.name, "price": self.price}
